d1493210bd514c7ccfe9eee215dac983bb37ab57
Anteconix <dutracm04@gmail.com>
2025-03-24 12:50:07 -0300

    novas tabelas

--- vortek/admin.py
+++ vortek/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Categoria, Editora
+from .models import Noticia,Comentario,Criptoativo,Criptoativo_adq
 
-admin.site.register(Categoria)
-admin.site.register(Editora)
+admin.site.register(Noticia)
+admin.site.register(Comentario)
+admin.site.register(Criptoativo)
+admin.site.register(Criptoativo_adq)
--- vortek/models.py
+++ vortek/models.py
@@ -1,13 +1,35 @@
 from django.db import models
 
-class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
+class Noticia(models.Model):
+    titulo_noticia = models.CharField(max_length=100)
+    conteudo_noticia = models.TextField()
+    ##fazer a imagem funfar 😢
     def __str__(self):
-        return self.descricao
-    
-class Editora(models.Model):
-    nome = models.CharField(max_length=100)
-    site = models.URLField(null=True, blank=True)
+        return self.titulo_noticia
 
+class Comentario(models.Model):
+##    nome_usuario = models.ForeignKey(
+##        User,
+##        on_delete=models.CASCADE,
+##        related_name="comentarios",
+##    )
+
+##      tem que ver isso também ☝😭
+    conteudo_comentario = models.TextField()
+##    def __str__(self):
+##        return self.nome_usuario
+
+class Criptoativo(models.Model):
+    cripto_sigla = models.CharField(max_length=10)
+    Criptoativo = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=5)
     def __str__(self):
-        return self.nome
+            return self.cripto_sigla
+
+class Criptoativo_adq(models.Model):
+    cripto_sigla = models.ForeignKey(Criptoativo, on_delete=models.CASCADE, related_name="cripto_siglas")
+    criptoativo = models.ForeignKey(Criptoativo, on_delete=models.CASCADE, related_name="criptoativos")
+    quant_adq = models.DecimalField(max_digits=10, decimal_places=5)
+    valor = models.ForeignKey(Criptoativo, on_delete=models.CASCADE, related_name="valors")
+    def __str__(self):
+        return self.cripto_sigla
