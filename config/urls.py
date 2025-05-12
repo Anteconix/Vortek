75d363f06033b9f506fffdb4b8925f3584a2e991
LocatelliBryann <bryannlocatelli@gmail.com>
2025-05-12 19:06:22 -0300

    adjust in user view

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 
-from vortek.views import NotiticaViewSet, ComentarioViewSet, CriptoativoViewSet, AporteViewSet
+from vortek.views import (
+    NotiticaViewSet,
+    ComentarioViewSet,
+    CriptoativoViewSet,
+    AporteViewSet,
+    UserView
+)
 
 router = DefaultRouter()
-router.register(r"noticias", NotiticaViewSet)   
+router.register(r"noticias", NotiticaViewSet)
 router.register(r"comentarios", ComentarioViewSet)
 router.register(r"criptoativos", CriptoativoViewSet)
 router.register(r"aportes", AporteViewSet)
@@ -18,6 +20,7 @@ router.register(r"aportes", AporteViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("api/user/", UserView.as_view()),  # <- Rota para pegar usuário autenticado
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- vortek/views.py
+++ vortek/views.py
@@ -1,15 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from vortek.models import Noticia, Comentario, Criptoativo, Aporte
 from vortek.serializers import NoticiaSerializer, ComentarioSerializer, CriptoativoSerializer, AporteSerializer
 
 class NotiticaViewSet(ModelViewSet):
     queryset = Noticia.objects.all()
     serializer_class = NoticiaSerializer
+
 class ComentarioViewSet(ModelViewSet):
     queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
     permission_classes = [IsAuthenticated]
+
 class CriptoativoViewSet(ModelViewSet):
     queryset = Criptoativo.objects.all()
     serializer_class = CriptoativoSerializer
@@ -17,3 +21,11 @@ class CriptoativoViewSet(ModelViewSet):
 class AporteViewSet(ModelViewSet):
     queryset = Aporte.objects.all()
     serializer_class = AporteSerializer
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "username": request.user.username
+        })
